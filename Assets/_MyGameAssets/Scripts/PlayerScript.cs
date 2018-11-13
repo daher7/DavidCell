22d5a66b20d5ec887c902be3d6cf15c5df101f2a
daher7 <daher7@gmail.com>
2018-11-13 13:34:24 +0100

    CircleHud

---
+++ Assets/_MyGameAssets/Scripts/PlayerScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerScript : MonoBehaviour {
+
+    NavMeshAgent agente;
+    public Transform targetCircle;
+
+    void Start() {
+        agente = GetComponent<NavMeshAgent>();
+    }
+
+    void Update() {
+        if (Input.GetButtonDown("Fire1")) {
+            ManageMouseClick();
+        }
+    }
+
+    private void ManageMouseClick() {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit rch;
+        bool hasTouch = Physics.Raycast(ray, out rch);
+        if (hasTouch) {
+            targetCircle.transform.position = rch.point;
+            targetCircle.transform.rotation = Quaternion.LookRotation(rch.normal);
+            agente.destination = targetCircle.transform.position;
+        }
+    }
+}
