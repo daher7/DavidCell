d661598aed4e3031aaa3aad476bff6436a6b41f8
daher7 <daher7@gmail.com>
2018-11-14 13:57:23 +0100

    NavMeshAgent

---
+++ Assets/_MyGameAssets/Scripts/EnemigoScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemigoScript : MonoBehaviour {
+
+    [SerializeField] Transform playerTransform;
+    NavMeshAgent;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.position = playerTransform.position;
+	}
+}
