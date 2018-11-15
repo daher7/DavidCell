1500614dd4d12f0f54d0f628b59861c76395143c
daher7 <daher7@gmail.com>
2018-11-15 13:58:48 +0100

    raycast

--- Assets/_MyGameAssets/Scripts/EnemigoScript.cs
+++ Assets/_MyGameAssets/Scripts/EnemigoScript.cs
@@ -2,23 +2,54 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.UI;
 
 public class EnemigoScript : MonoBehaviour {
 
+    public Text textDTP, textATP;
+    public GameObject player;
     [SerializeField] Transform[] puntosPatrulla = new Transform[4];
     NavMeshAgent agente;
     // Tiempo de espera entre asignaciones de puntos de patrulla
-    const int TIEMPO_ESPERA = 1; 
+    const int TIEMPO_ESPERA = 1;
     // DECLARACION DE ESTADO
     enum Estado { Idle, Andando, Corriendo, Saltando, Disparando };
     Estado estado = Estado.Idle;
 
     private void Start() {
         agente = GetComponent<NavMeshAgent>();
-        AsignarPuntoPatrulla();
+        //AsignarPuntoPatrulla();
     }
 
     private void Update() {
+
+        float distanciaAlplayer = Vector3.Distance(transform.position, player.transform.position);
+        Vector3 direccion = Vector3.Normalize(player.transform.position - transform.position);
+        float anguloAlPlayer = Vector3.Angle(direccion, transform.forward);
+        // Vamos a comprobar si ve al player
+        if (distanciaAlplayer < 7 && anguloAlPlayer < 25) {
+
+            // Lanzamos un raycast para ver si hay no hay ningun obstaculo en la linea de vision
+            Debug.DrawLine(transform.position, player.transform.position, Color.red, 1);
+            RaycastHit rh;
+            bool hayImpacto = Physics.Raycast(
+                transform.position,
+                direccion,
+                out rh, 
+                Mathf.Infinity);
+           
+           
+            if (hayImpacto) {
+                //string nombreObjetoImpactado = rh.transform.gameObject.name;
+                print(rh.transform.gameObject.name);
+            } 
+            
+            print(hayImpacto);
+        }
+        
+        textDTP.text = "DTP: " + distanciaAlplayer.ToString();
+        textATP.text = "ATP: " + anguloAlPlayer.ToString();
+
         // Evaluacion de los estados
         switch (estado) {
             case Estado.Idle:
@@ -38,7 +69,7 @@ public class EnemigoScript : MonoBehaviour {
                 break;
         }
     }
-    
+
     private void ComprobarDestino() {
         if (!agente.pathPending) {
             if (agente.remainingDistance <= agente.stoppingDistance + 0.1) {
