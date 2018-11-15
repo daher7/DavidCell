24f9bd01e7ccdee3657358d06a593ccea7c7a1a7
daher7 <daher7@gmail.com>
2018-11-15 10:00:17 +0100

    MarujaPatrulla

--- Assets/_MyGameAssets/Scripts/EnemigoScript.cs
+++ Assets/_MyGameAssets/Scripts/EnemigoScript.cs
@@ -5,16 +5,67 @@ using UnityEngine.AI;
 
 public class EnemigoScript : MonoBehaviour {
 
-    [SerializeField] Transform playerTransform;
-    NavMeshAgent;
+    
+    [SerializeField] Transform[] puntosPatrulla = new Transform[4];
+    NavMeshAgent agente;
+    // Tiempo de espera entre asignaciones de puntos de patrulla
+    const int TIEMPO_ESPERA = 1; 
+    // DECLARACION DE ESTADO
+    enum Estado { Idle, Andando, Corriendo, Saltando, Disparando };
+    Estado estado = Estado.Idle;
 
-    // Use this for initialization
-    void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        this.transform.position = playerTransform.position;
-	}
+    private void Start() {
+        agente = GetComponent<NavMeshAgent>();
+        AsignarPuntoPatrulla();
+    }
+
+    private void Update() {
+        // Evaluacion de los estados
+        switch (estado) {
+            case Estado.Idle:
+                // NO EVALUAMOS NADA
+                break;
+            case Estado.Andando:
+                ComprobarDestino();
+                break;
+            case Estado.Corriendo:
+
+                break;
+            case Estado.Saltando:
+
+                break;
+            case Estado.Disparando:
+
+                break;
+        }
+    }
+    
+    private void ComprobarDestino() {
+        if (!agente.pathPending) {
+            if (agente.remainingDistance <= agente.stoppingDistance) {
+                // animador.SetBool("andando", false);
+                estado = Estado.Idle;
+                Invoke("AsignarPuntoPatrulla", TIEMPO_ESPERA);
+            }
+        }
+    }
+    // PUNTOS DE PATRULLA ALEATORIOS
+    private void AsignarPuntoPatrulla() {
+        int pp = Random.Range(0, puntosPatrulla.Length);
+        agente.destination = puntosPatrulla[pp].position;
+        estado = Estado.Andando;
+    }
+
+    // PUNTOS DE PATRULLA SECUENCIAL
+    /*
+    int pp = 0;
+    private void AsignarPuntoPatrulla() {
+        if(pp == puntosPatrulla.Length) {
+            pp = 0;
+        }
+        agente.destination = puntosPatrulla[0].position;
+        estado = Estado.Andando;
+        pp++;
+    }
+    */
 }
